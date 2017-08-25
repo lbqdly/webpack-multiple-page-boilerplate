d3097a8329de3ba059364e48422770e79aa2d829
good318 <304469323@qq.com>
2017-08-25 10:59:46 +0800

    show dist/

---
+++ dist/js/init.js
@@ -0,0 +1,18 @@
+webpackJsonp([3],{
+
+/***/ 196:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(86);
+
+
+/***/ }),
+
+/***/ 86:
+/***/ (function(module, exports) {
+
+eval("console.log('init');//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9jb21tb25zL2luaXQuanM/OWY0NiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQUEsUUFBUUMsR0FBUixDQUFZLE1BQVoiLCJmaWxlIjoiODYuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaW5pdCcpO1xyXG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9jb21tb25zL2luaXQuanMiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///86\n");
+
+/***/ })
+
+},[196]);
